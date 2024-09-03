b3f243cf6cd07f22a12044364a55af437807788a
k.kondakov <k.kondakov@dats.team>
2024-09-03 17:16:31 +0300

    hw05

--- hw05_shapes/figure/pkg.go
+++ hw05_shapes/figure/pkg.go
@@ -6,17 +6,13 @@ type Shape interface {
 	Area() (float64, error)
 }
 
-func CalculateArea(s any) (float64, error) {
+func CalculateArea(s any) (area float64, areaError error) {
 	shape, ok := s.(Shape)
 	if !ok {
 		return 0, errors.New("переданный объект не фигура или фигура без площади")
 	}
 
-	area, areaError := shape.Area()
+	area, areaError = shape.Area()
 
-	if areaError != nil {
-		return 0, areaError
-	}
-
-	return area, nil
+	return
 }
