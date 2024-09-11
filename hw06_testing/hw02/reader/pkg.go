d7c49f1ca8536746bdac7bd35113f1d7399012bb
k.kondakov <k.kondakov@dats.team>
2024-09-11 21:31:54 +0300

    test

--- hw06_testing/hw02/reader/pkg.go
+++ hw06_testing/hw02/reader/pkg.go
@@ -12,25 +12,19 @@ import (
 func ReadJSON(filePath string) ([]types.Employee, error) {
 	f, err := os.Open(filePath)
 	if err != nil {
-		fmt.Printf("Error: %v", err)
-
-		return nil, err
+		return nil, fmt.Errorf("open json file: %w", err)
 	}
 
 	bytes, err := io.ReadAll(f)
 	if err != nil {
-		fmt.Printf("Error: %v", err)
-
-		return nil, err
+		return nil, fmt.Errorf("read json file: %w", err)
 	}
 
 	var data []types.Employee
 
 	err = json.Unmarshal(bytes, &data)
 	if err != nil {
-		fmt.Printf("Error: %v", err)
-
-		return nil, err
+		return nil, fmt.Errorf("unmarshal json string: %w", err)
 	}
 
 	return data, nil
