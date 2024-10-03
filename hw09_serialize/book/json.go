a8c9ec676a31140b5306d18fa97552d789f08a67
k.kondakov <k.kondakov@dats.team>
2024-10-03 23:50:42 +0300

    hw09

--- hw09_serialize/book/json.go
+++ hw09_serialize/book/json.go
@@ -2,6 +2,7 @@ package book
 
 import (
 	"encoding/json"
+	"strings"
 )
 
 func (b *Book) MarshalJSON() ([]byte, error) {
@@ -49,4 +50,68 @@ func (b *Book) UnmarshalJSON(data []byte) error {
 	b.Rate = tempStruct.Rate
 
 	return nil
+}
+
+func MarshalBookSlice(books []Book) ([]byte, error) {
+	var b strings.Builder
+
+	_, startErr := b.WriteString("[")
+	if startErr != nil {
+		return nil, startErr
+	}
+
+	first := true
+	for _, el := range books {
+		if first {
+			first = false
+		} else {
+			_, err := b.WriteString(",")
+			if err != nil {
+				return nil, err
+			}
+		}
+		jsonString, mErr := el.MarshalJSON()
+		if mErr != nil {
+			return nil, mErr
+		}
+		b.WriteString(string(jsonString))
+	}
+
+	_, endErr := b.WriteString("]")
+	if endErr != nil {
+		return nil, endErr
+	}
+
+	return []byte(b.String()), nil
+}
+
+func UnmarshalBookSliceJSON(data []byte) ([]Book, error) {
+	tempStruct := []struct {
+		ID     uint32  `json:"id"`
+		Title  string  `json:"title"`
+		Author string  `json:"author"`
+		Year   uint32  `json:"year"`
+		Size   uint32  `json:"size"`
+		Rate   float32 `json:"rate"`
+	}{}
+	err := json.Unmarshal(
+		data,
+		&tempStruct,
+	)
+	if err != nil {
+		return nil, err
+	}
+	result := make([]Book, len(tempStruct))
+	for index, ts := range tempStruct {
+		result[index] = Book{
+			ID:     ts.ID,
+			Title:  ts.Title,
+			Author: ts.Author,
+			Year:   ts.Year,
+			Size:   ts.Size,
+			Rate:   ts.Rate,
+		}
+	}
+
+	return result, nil
 }
--- hw09_serialize/book/json_test.go
+++ hw09_serialize/book/json_test.go
@@ -6,21 +6,43 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestBookMarshalJSON(t *testing.T) {
-	testCases := []struct {
-		name string
-		book Book
-		json []byte
-	}{
-		{"empty", Book{}, []byte(`{"id":0,"title":"","author":"","year":0,"size":0,"rate":0}`)},
-		{
-			"not empty",
-			Book{ID: 123, Title: "title", Author: "author", Year: 2000, Size: 100, Rate: 1.6},
-			[]byte(`{"id":123,"title":"title","author":"author","year":2000,"size":100,"rate":1.6}`),
-		},
-	}
+var simpleCases = []struct {
+	name string
+	book Book
+	json []byte
+}{
+	{"empty", Book{}, []byte(`{"id":0,"title":"","author":"","year":0,"size":0,"rate":0}`)},
+	{
+		"not empty",
+		Book{ID: 123, Title: "title", Author: "author", Year: 2000, Size: 100, Rate: 1.6},
+		[]byte(`{"id":123,"title":"title","author":"author","year":2000,"size":100,"rate":1.6}`),
+	},
+}
 
-	for _, tc := range testCases {
+var sliceCases = []struct {
+	name       string
+	bookSlice  []Book
+	jsonString []byte
+}{
+	{"empty", []Book{}, []byte("[]")},
+	{
+		"one element",
+		[]Book{{ID: 123, Title: "title", Author: "author", Year: 2000, Size: 100, Rate: 1.6}},
+		[]byte(`[{"id":123,"title":"title","author":"author","year":2000,"size":100,"rate":1.6}]`),
+	},
+	{
+		"two elements",
+		[]Book{
+			{ID: 123, Title: "title", Author: "author", Year: 2000, Size: 100, Rate: 1.6},
+			{},
+		},
+		[]byte(`[{"id":123,"title":"title","author":"author","year":2000,"size":100,"rate":1.6}` +
+			`,{"id":0,"title":"","author":"","year":0,"size":0,"rate":0}]`),
+	},
+}
+
+func TestBookMarshalJSON(t *testing.T) {
+	for _, tc := range simpleCases {
 		t.Run(tc.name, func(t *testing.T) {
 			res, err := tc.book.MarshalJSON()
 			assert.Equal(t, tc.json, res)
@@ -30,19 +52,7 @@ func TestBookMarshalJSON(t *testing.T) {
 }
 
 func TestBookUnmarshalJSON(t *testing.T) {
-	testCases := []struct {
-		name string
-		book Book
-		json []byte
-	}{
-		{"empty", Book{}, []byte(`{"id":0,"title":"","author":"","year":0,"size":0,"rate":0}`)},
-		{
-			"not empty",
-			Book{ID: 123, Title: "title", Author: "author", Year: 2000, Size: 100, Rate: 1.6},
-			[]byte(`{"id":123,"title":"title","author":"author","year":2000,"size":100,"rate":1.6}`),
-		},
-	}
-	for _, tc := range testCases {
+	for _, tc := range simpleCases {
 		t.Run(tc.name, func(t *testing.T) {
 			b := Book{}
 			err := b.UnmarshalJSON(tc.json)
@@ -50,4 +60,37 @@ func TestBookUnmarshalJSON(t *testing.T) {
 			assert.Nil(t, err)
 		})
 	}
+
+	t.Run("malformed json", func(t *testing.T) {
+		b := Book{}
+		err := b.UnmarshalJSON([]byte("sdfdfhawer"))
+		assert.Equal(t, Book{}, b)
+		assert.NotNil(t, err)
+	})
+}
+
+func TestMarshalBookSlice(t *testing.T) {
+	for _, tc := range sliceCases {
+		t.Run(tc.name, func(t *testing.T) {
+			actualString, err := MarshalBookSlice(tc.bookSlice)
+			assert.Equal(t, tc.jsonString, actualString)
+			assert.Nil(t, err)
+		})
+	}
+}
+
+func TestUnmarshalBookSliceJSON(t *testing.T) {
+	for _, tc := range sliceCases {
+		t.Run(tc.name, func(t *testing.T) {
+			actualSlice, err := UnmarshalBookSliceJSON(tc.jsonString)
+			assert.Equal(t, tc.bookSlice, actualSlice)
+			assert.Nil(t, err)
+		})
+	}
+
+	t.Run("malformed json", func(t *testing.T) {
+		actualSlice, err := UnmarshalBookSliceJSON([]byte("adgsdf"))
+		assert.Nil(t, actualSlice)
+		assert.NotNil(t, err)
+	})
 }
