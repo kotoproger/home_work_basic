2fc008d86d15dbdbddc230240141b2939b6eeb17
k.kondakov <k.kondakov@dats.team>
2024-10-04 00:11:57 +0300

    hw09

--- hw09_serialize/book/json.go
+++ hw09_serialize/book/json.go
@@ -5,49 +5,61 @@ import (
 	"strings"
 )
 
+type jsonBook struct {
+	ID     uint32  `json:"id"`
+	Title  string  `json:"title"`
+	Author string  `json:"author"`
+	Year   uint32  `json:"year"`
+	Size   uint32  `json:"size"`
+	Rate   float32 `json:"rate"`
+}
+
+func (jb *jsonBook) book() Book {
+	return Book{
+		ID:     jb.ID,
+		Title:  jb.Title,
+		Author: jb.Author,
+		Year:   jb.Year,
+		Size:   jb.Size,
+		Rate:   jb.Rate,
+	}
+}
+
+func (jb *jsonBook) toBook(b *Book) {
+	b.ID = jb.ID
+	b.Title = jb.Title
+	b.Author = jb.Author
+	b.Year = jb.Year
+	b.Size = jb.Size
+	b.Rate = jb.Rate
+}
+
+func (jb *jsonBook) fromBook(b *Book) {
+	jb.ID = b.ID
+	jb.Title = b.Title
+	jb.Author = b.Author
+	jb.Year = b.Year
+	jb.Size = b.Size
+	jb.Rate = b.Rate
+}
+
 func (b *Book) MarshalJSON() ([]byte, error) {
-	return json.Marshal(
-		struct {
-			ID     uint32  `json:"id"`
-			Title  string  `json:"title"`
-			Author string  `json:"author"`
-			Year   uint32  `json:"year"`
-			Size   uint32  `json:"size"`
-			Rate   float32 `json:"rate"`
-		}{
-			b.ID,
-			b.Title,
-			b.Author,
-			b.Year,
-			b.Size,
-			b.Rate,
-		},
-	)
+	jBook := jsonBook{}
+	jBook.fromBook(b)
+	return json.Marshal(jBook)
 }
 
 func (b *Book) UnmarshalJSON(data []byte) error {
-	tempStruct := struct {
-		ID     uint32  `json:"id"`
-		Title  string  `json:"title"`
-		Author string  `json:"author"`
-		Year   uint32  `json:"year"`
-		Size   uint32  `json:"size"`
-		Rate   float32 `json:"rate"`
-	}{}
+	jBook := jsonBook{}
 	err := json.Unmarshal(
 		data,
-		&tempStruct,
+		&jBook,
 	)
 	if err != nil {
 		return err
 	}
 
-	b.ID = tempStruct.ID
-	b.Title = tempStruct.Title
-	b.Author = tempStruct.Author
-	b.Year = tempStruct.Year
-	b.Size = tempStruct.Size
-	b.Rate = tempStruct.Rate
+	jBook.toBook(b)
 
 	return nil
 }
@@ -86,14 +98,7 @@ func MarshalBookSlice(books []Book) ([]byte, error) {
 }
 
 func UnmarshalBookSliceJSON(data []byte) ([]Book, error) {
-	tempStruct := []struct {
-		ID     uint32  `json:"id"`
-		Title  string  `json:"title"`
-		Author string  `json:"author"`
-		Year   uint32  `json:"year"`
-		Size   uint32  `json:"size"`
-		Rate   float32 `json:"rate"`
-	}{}
+	tempStruct := []jsonBook{}
 	err := json.Unmarshal(
 		data,
 		&tempStruct,
@@ -103,14 +108,7 @@ func UnmarshalBookSliceJSON(data []byte) ([]Book, error) {
 	}
 	result := make([]Book, len(tempStruct))
 	for index, ts := range tempStruct {
-		result[index] = Book{
-			ID:     ts.ID,
-			Title:  ts.Title,
-			Author: ts.Author,
-			Year:   ts.Year,
-			Size:   ts.Size,
-			Rate:   ts.Rate,
-		}
+		result[index] = ts.book()
 	}
 
 	return result, nil
