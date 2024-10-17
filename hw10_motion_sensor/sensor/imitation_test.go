028f2c43a9fd21dbcafa78d912e52b1c355020f7
k.kondakov <k.kondakov@dats.team>
2024-10-17 18:07:41 +0300

    hw10 add tests on buffered channel

--- hw10_motion_sensor/sensor/imitation_test.go
+++ hw10_motion_sensor/sensor/imitation_test.go
@@ -30,3 +30,29 @@ func TestReadSensor(t *testing.T) {
 		)
 	}
 }
+
+func TestReadSensorWithoutBuffer(t *testing.T) {
+	testCases := []struct {
+		name           string
+		durationMs     int
+		freqMS         int64
+		bufferLength   int
+		expectedLength int
+	}{
+		{name: "without buffer", durationMs: 10, freqMS: 3, bufferLength: 0, expectedLength: 0},
+		{name: "with buffer", durationMs: 10, freqMS: 3, bufferLength: 1, expectedLength: 1},
+	}
+
+	for _, testCase := range testCases {
+		t.Run(
+			testCase.name,
+			func(t *testing.T) {
+				ch := make(chan int, testCase.bufferLength)
+				defer close(ch)
+				duration := time.After(time.Millisecond * time.Duration(testCase.durationMs))
+				ReadSensor(ch, duration, time.Millisecond*3)
+				assert.Equal(t, testCase.expectedLength, len(ch))
+			},
+		)
+	}
+}
